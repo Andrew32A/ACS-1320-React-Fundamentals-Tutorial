48483ec0baee5709239390ba4da75374d89d51a1
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-19 23:16:20 -0800

    added styling to spaces and details

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -6,17 +6,19 @@ function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     
     return (
-        <div>
-        <Link to={`/details/${id}`}>
-            <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-        </Link>
-        <h1>
+        <div className="POPOSSpace">
             <Link to={`/details/${id}`}>
-                {name}
+                <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
             </Link>
-        </h1>
-        <div>{address}</div>
-        <div>{hours}</div>
+            <h1>
+                <Link className="POPOSSpace-title" to={`/details/${id}`}>
+                    {name}
+                </Link>
+            </h1>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div>{hours}</div>
+            </div>
         </div>
     )
 }
